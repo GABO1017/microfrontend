368ae245d7f075a67428b7d58f03eb9c2810e311
GABO1017 <gabrielfranco1017@gmail.com>
2023-11-20 01:48:18 -0500

    store webpack and microfronted full funcionality

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { storeTodo, ITodo } from '@Renthub/store';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular';
+
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      title: new FormControl(null, Validators.required)
+    })
+
+  }
+
+  onSubmit(): void {
+    
+    const {title} = this.form.value;
+
+    const todo: ITodo = {
+      completed: true,
+      id: storeTodo.id,
+      text: title,
+    };
+
+    storeTodo.addTodo(todo);
+
+    this.form.reset();
+    
+  }
 }
--- mf-react/src/root.component.tsx
+++ mf-react/src/root.component.tsx
@@ -1,3 +1,24 @@
+import { useEffect, useState } from "react";
+import { storeTodo, ITodo } from "@Renthub/store";
+
 export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+
+  const [todos, setTodos] = useState<ITodo[]>([])
+
+  useEffect(() => {
+
+    const subscription = storeTodo.storeTodo$.subscribe(setTodos)
+    
+    return () => {
+      subscription.unsubscribe();
+    };
+  },[]);
+
+  return <section>
+    <ul>
+      {todos.map(todo => (
+        <li key={todo.id}>{todo.text}</li>
+      ))}
+      </ul>  
+  </section>;
 }
