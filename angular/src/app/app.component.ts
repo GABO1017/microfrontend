b5042e6e99295e620317cb98dc9c61b0981437e9
GABO1017 <gabrielfranco1017@gmail.com>
2023-11-20 23:13:04 -0500

    Styles and routing

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -14,6 +14,66 @@ export class AppComponent {
 
   form: FormGroup;
 
+
+  navbarContainer = {
+    'width': '100%',
+    'height': '10vh',
+    'background-color': 'lavender',
+    'position': 'fixed',
+    'top': '0',
+    'left': '0'
+  };
+  
+ 
+  color = {
+    'display': 'flex',
+    'justify-content': 'center',
+    'padding': '3vh'
+  };
+  
+  navLink = {
+    'margin': '0 10vh', 
+    'text-decoration': 'none',
+    'color': '#333',
+    'font-weight': 'bold',
+    'font-size': '30px'
+  };
+
+  inputStyles = {
+    'padding': '10px',
+    
+    'border': '1px solid #ccc',
+    'border-radius': '5px',
+    'width': '200px' 
+  };
+
+  
+  buttonStyles = {
+    'padding': '10px',
+    'margin-left': '10px',
+    'background-color': '#4CAF50',
+    'color': 'white',
+    'border': 'none',
+    'border-radius': '5px',
+    'cursor': 'pointer'
+  };
+
+  container = {
+    'display': 'flex',
+    'justify-content': 'center',
+    'align-items': 'center',
+    'height': '37vh' 
+  };
+  
+
+  centeredDiv = {
+    'width': '30%', 
+    'padding': '20px',
+    'border': '1px solid #ccc',
+    'border-radius': '5px',
+    'background-color': 'azure'
+  };
+
   constructor(private fb: FormBuilder) {
     this.form = this.fb.group({
       title: new FormControl(null, Validators.required)
--- mf-react/src/root.component.tsx
+++ mf-react/src/root.component.tsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import { storeTodo, ITodo } from "@Renthub/store";
+import './styles.css'
 
 export default function Root(props) {
 
@@ -14,11 +17,28 @@ export default function Root(props) {
     };
   },[]);
 
+  const handleDelete = (id: number) => {
+    storeTodo.deleteTodo(id);
+  };
+
   return <section>
-    <ul>
-      {todos.map(todo => (
-        <li key={todo.id}>{todo.text}</li>
-      ))}
-      </ul>  
+    <div className="navbar-container">
+      <div className="color">
+        <a href="/" className="nav-link">Angular Form</a>
+        <a href="/react" className="nav-link">React List</a>
+      </div>
+    </div>
+    <div className="todo-list-container">
+      <h1>List of To Do</h1>
+      <ul className="todo-list">
+        {todos.map(todo => (
+          <li key={todo.id}>{todo.text}
+            <button onClick={() => handleDelete(todo.id)}> 
+              <FontAwesomeIcon icon={faTimes} />
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
   </section>;
 }
